59a8af94b910cfb0e9c10c284de5ee382496fbea
Andrey Zhuravlyov <crane.zhuravlyov@gmail.com>
2020-11-26 18:03:12 +0300

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5fbfc2ce08b41a001fec7a5d",
+				"5fbfc2ce08b41a001fec7a5f"
+			]
+		},
+		"5fbfc2ce08b41a001fec7a5d": {
+			"id": "5fbfc2ce08b41a001fec7a5d",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5fbfc2ce08b41a001fec7a5f": {
+			"id": "5fbfc2ce08b41a001fec7a5f",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby"
+}
